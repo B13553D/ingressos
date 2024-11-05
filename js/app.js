094dc6b9ba70327796f7b69f91612f946112aca6
B13353D <lipecsgo995@gmail.com>
2024-11-05 18:53:35 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+function comprar(){
+    let tipoIngresso = document.getElementById('tipo-ingresso');
+    let quantidade = parseInt(document.getElementById('qtd').value);
+    
+    if(tipoIngresso.value == 'pista'){
+        comprarPista(quantidade);
+    } if(tipoIngresso.value == 'inferior'){
+        comprarInferior(quantidade);
+    } if(tipoIngresso.value == 'superior'){
+        comprarSuperior(quantidade);
+    }
+}
+
+function comprarPista(quantidade){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if(quantidade > qtdPista){
+        alert('Quantidade indisponível!');
+    } else {
+        qtdPista = qtdPista - quantidade;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarInferior(quantidade){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if(quantidade > qtdInferior){
+        alert('Quantidade indisponível!');
+    } else {
+        qtdInferior = qtdInferior - quantidade;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarSuperior(quantidade){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if(quantidade > qtdSuperior){
+        alert('Quantidade indisponível!');
+        comprarIngressosRestantes();
+    } else {
+        qtdSuperior = qtdSuperior - quantidade;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarIngressosRestantes(){
+    let confirmação = confirm('Você deseja comprar a quantia restante?');
+    if(confirmação == true){
+        parseInt(document.getElementById('qtd').value) = qtdSuperior;
+        return comprarSuperior();
+        
+    }
+}
